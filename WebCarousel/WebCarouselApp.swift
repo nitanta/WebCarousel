a524810e3cd0bae65c5e1803dca948f74b233151
Nitanta Adhikari <nitanta.adh007@gmail.com>
2023-08-18 15:51:55 +0545

    Initial Commit

---
+++ WebCarousel/WebCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebCarouselApp.swift
+//  WebCarousel
+//
+//  Created by Nitanta Adhikari on 18/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct WebCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
